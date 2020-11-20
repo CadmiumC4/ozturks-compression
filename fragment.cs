0ec514d71f6348d7b9a387d9fa38e3e1a7de0ec0
Yiğit Cemal Öztürk <ozturkyigitcemal@outlook.com.tr>
2020-11-20 17:04:26 +0300

    Update fragment.cs
    Now the fragmentation algorithm is useful.

--- fragment.cs
+++ fragment.cs
@@ -10,6 +10,18 @@ namespace CadmiumC4.IO.Compression
 {
    partial class Compote
    {
+       public Compote()
+       {
+       }
+       public Compote(DirectoryInfo tempFolder)
+       {
+           TemporaryFolder = tempFolder;
+       }
+       public DirectoryInfo TemporaryFolder
+       {
+           get;
+           set; //maybe be replaced with 'init'.
+       }
        protected int CalculateForce(int force)
        {
             /* variables for compression
@@ -35,19 +47,51 @@ namespace CadmiumC4.IO.Compression
             return forceval;
 
        }
-       public void FragmentFile(FileStream file, int power)
+       private void FragmentFile(FileStream file, int power)
        {
            /*The fragment array.*/
             byte[] bytes;
-            /*Let the file array*/
-            FileInfo[] files;
+            /*Let the fragment file array*/
+            FileInfo[] fragments;
+           /*fragment enumerator*/
+           int fragment;
+           //Let a temporary variable to keep the fragment.
+            FileStream fragFile;
            /*First of all, we need to calculate the exponent.*/
             int byteCount = CalculateForce(power);
             /*and the fragment count.*/
-            
-
+            int fragmentCount = file.Length / CalculateForce(power);
+            /*lastly, add the file-name array*/
+           string[] fileNames = new string[fragmentCount];
            /*begin the fragmentation of the file*/
-           //while()
+           for(fragment = 0;fragment < fragmentCount;fragment++)
+           {
+               //set the name of the fragment.
+               fileNames[fragment] = string.Format(@"{0}\{1:X}",temporaryFolder.FullName,fragment);
+           }
+           //Create the files(virtually).
+           for(fragment = 0; fragment < fragmentCount;fragment++)
+           {
+               fragments[fragment] = new FileInfo(fileNames[fragment]);
+           }
+           //Now, create the files really and write the fragments.
+           for(fragment = 0; fragment < fragmentFile; fragment++)
+           {
+               fragFile = fragments[fragment].Create();
+               //debug line
+               if(!(fragFile.CanWrite && file.CanRead))
+                   throw new IOException("Access to the file is denied.");
+               //create the byte array.
+               bytes = new byte[byteCount];
+               
+               //Read bytes from the file.
+               file.Read(bytes,0, byteCount);
+               // And write them into the fragment.
+               fragFile.Write(bytes,0,byteCount);
+               
+               //Close the fragment file.
+               fragFile.Close();
+           }
        }
 
    }
