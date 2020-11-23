7fa468eb6636a41f1f79bc32b0cffcf9ed661486
Yiğit Cemal Öztürk <ozturkyigitcemal@outlook.com.tr>
2020-11-23 12:40:53 +0300

    Update constructors.cs

--- constructors.cs
+++ constructors.cs
@@ -1,3 +1,17 @@
 using System;
 using System.IO;
 using System.IO.Compression;
+
+namespace CadmiumC4.IO.Compression
+{
+  partial class Compote
+  {
+    public Compote(int force)
+    {
+      _force = CalculateForce(force);
+    }
+    public Compote():this(3){}
+  }
+  
+  
+}
