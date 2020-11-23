d93585e641e1f649d676f865c754fd5e4bb40987
Yiğit Cemal Öztürk <ozturkyigitcemal@outlook.com.tr>
2020-11-23 14:00:00 +0300

    Create fragment-async.cs

---
+++ fragment-async.cs
@@ -0,0 +1,12 @@
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+
+namespace CadmiumC4.IO.Compression
+{
+  partial class Compote
+  {
+      public extern int 
+  }
+}
