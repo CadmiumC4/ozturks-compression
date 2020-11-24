02a39d47044e58d079a0bfd6d271544fe23b6ff7
Yiğit Cemal Öztürk <ozturkyigitcemal@gmail.com>
2020-11-24 10:03:50 +0300

    Add files via upload

---
+++ ozturks-compression/fragment-async.cs
@@ -0,0 +1,74 @@
+/*
+
+  Module of asynchronous fragmenting.
+
+*/
+
+
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+
+namespace CadmiumC4.IO.Compression
+{
+  partial class Compote
+  {
+      public extern int Force
+      {
+        get;
+        set;
+      }
+      public extern DirectoryInfo TemporaryFolder
+      {
+        get;
+        set;
+      }
+    
+      private async void FragmentFileAsync(FileStream file)
+       {
+           /*The fragment array.*/
+            byte[] bytes;
+            /*Let the fragment file array*/
+            FileInfo[] fragments;
+           /*fragment enumerator*/
+           int fragment;
+           //Let a temporary variable to keep the fragment.
+            FileStream fragFile;
+           /*First of all, we need to calculate the exponent.*/
+            int byteCount = _forceField;
+            /*and the fragment count.*/
+            int fragmentCount = file.Length / _forceField;
+            /*lastly, add the file-name array*/
+           string[] fileNames = new string[fragmentCount];
+           /*begin the fragmentation of the file*/
+           for(fragment = 0;fragment < fragmentCount;fragment++)
+           {
+               //set the name of the fragment.
+               fileNames[fragment] = string.Format(@"{0}\{1:X}",temporaryFolder.FullName,fragment);
+           }
+           //Create the files(virtually).
+           for(fragment = 0; fragment < fragmentCount;fragment++)
+           {
+               fragments[fragment] = new FileInfo(fileNames[fragment]);
+           }
+           //Now, create the files really and write the fragments.
+           for(fragment = 0; fragment < fragmentFile; fragment++)
+           {
+               fragFile = fragments[fragment].Create();
+               //debug line
+               if(!(fragFile.CanWrite && file.CanRead))
+                   throw new IOException("Access to the file is denied.");
+               //create the byte array.
+               bytes = new byte[byteCount];
+               
+               //Read bytes from the file.
+               await file.ReadAsync(bytes,0, byteCount);
+               // And write them into the fragment.
+               await fragFile.WriteAsync(bytes,0,byteCount);
+               
+               //Close the fragment file.
+               fragFile.Close();
+           }
+  }
+}
--- ozturks-compression/ozturks-compression/README.md
+++ ozturks-compression/ozturks-compression/README.md
@@ -5,7 +5,3 @@ The repository for Öztürk's Compression
 Öztürk's Compression is a compression model which adapts a large-sized file to the purpose of an archive.
 This is an open source project - after I add some code to it.
 More details are on [the wiki](https://github.com/CadmiumC48/ozturks-compression/wiki).
-
-Please ignore the files in [ozturks-compression](https://github.com/CadmiumC48/ozturks-compression/tree/main/ozturks-compression) that are not written in C#.
-They were added by the Git CLI and they are not related to compression.
-(this is why Git is called "the stupid content tracker".)
