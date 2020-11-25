119560dc1a9f8dba3dae6ddb696618a94e8e4f90
Yiğit Cemal Öztürk <ozturkyigitcemal@outlook.com.tr>
2020-11-25 15:23:16 +0300

    Create compress.cs
    Compression project is finally started.

---
+++ modules/final/compress.cs
@@ -0,0 +1,20 @@
+using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace CadmiumC4.IO.Compression
+{
+    partial class Compote
+    {
+          public partial DirectoryInfo TemporaryDirectory
+          {
+          }
+          ///<summary>
+          ///Compresses a file.
+          ///</summary>
+          public void Compress(FileInfo file)
+          {
+            
+          }
+    }
+}
