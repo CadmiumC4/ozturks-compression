bcbf1322543ffedc971f904ab518ae17b009ab6f
Yiğit Cemal Öztürk <ozturkyigitcemal@outlook.com.tr>
2020-11-25 15:11:55 +0300

    Update concatenation.cs
    The target file is now accepted as a FileInfo object.

--- modules/concatenation/concatenation.cs
+++ modules/concatenation/concatenation.cs
@@ -17,13 +17,14 @@ namespace CadmiumC4.IO.Compression
 		{
 			
 		}
-		protected void ConcatFiles(FileStream target)
+		protected void ConcatFiles(FileInfo target, string inputFilePath)
 		{
-			FileInfo[] _files = TemporaryFolder.GetFiles();
+			FileInfo[] _files = (new DirectoryInfo($@"{TemporaryFolder.FullPath}\{inputFilePath}")).GetFiles();
 			FileStream _partStream;
+			FileStream _targetStream = target.OpenWrite():
 			byte[] _fragment = new byte[_force];
 			
-			if(!target.CanWrite)
+			if(!_targetStream.CanWrite)
 				throw new IOException("File is not writeable! Access denied to file.");
 			
 			foreach(FileInfo _file in _files)
@@ -31,11 +32,12 @@ namespace CadmiumC4.IO.Compression
 				_partStream = _file.Open(FileMode.Open, FileAccess.Read);
 				_partStream.Read(_fragment,0,_force);
 				
-				target.Write(_fragment,0,_force);
+				_targetStream.Write(_fragment,0,_force);
 				
 				_partStream.Close();
 			}
 			
+		  _targetStream.Close();	
 			
 		}
 	}
