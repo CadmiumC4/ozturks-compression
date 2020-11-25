20a28c6e83f6119ce7a6829d44679fba124bcfdd
Yiğit Cemal Öztürk <ozturkyigitcemal@outlook.com.tr>
2020-11-25 15:34:14 +0300

    Create decompress.cs

---
+++ modules/final/decompress.cs
@@ -0,0 +1,18 @@
+using System;
+using System.IO.Compression;
+using System.IO;
+
+namespace CadmiumC4.IO.Compression
+{
+  partial class Compote
+  {
+      partial DirectoryInfo TemporaryDirectory
+      {
+      }
+      
+      public void Decompress(FileInfo inputFile,FileInfo outputFile)
+      {
+          
+      }
+  }
+}
