05b5b7af9e75a14c192466ecf5cb6081ae061047
Yiğit Cemal Öztürk <ozturkyigitcemal@outlook.com.tr>
2020-11-25 15:15:46 +0300

    Update concatenation_async.cs
    The file is now accepted as a FileInfo object.

--- modules/concatenation/concatenation_async.cs
+++ modules/concatenation/concatenation_async.cs
@@ -18,13 +18,14 @@ namespace CadmiumC4.IO.Compression
 		{
 			
 		}
-		protected async void ConcatFiles(FileStream target)
+		protected async void ConcatFiles(FileInfo target,string partFilePath)
 		{
-			FileInfo[] _files = TemporaryFolder.GetFiles();
+			FileInfo[] _files = (new DirectoryInfo(@$"{TemporaryFolder.FullPath}\{partFilePath}")).GetFiles();
 			FileStream _partStream;
+			FileStream _targetStream = target.OpenWrite();
 			byte[] _fragment = new byte[_force];
 			
-			if(!target.CanWrite)
+			if(!_targetStream.CanWrite)
 				throw new IOException("File is not writeable! Access denied to file.");
 			
 			foreach(FileInfo _file in _files)
@@ -32,12 +33,12 @@ namespace CadmiumC4.IO.Compression
 				_partStream = _file.Open(FileMode.Open, FileAccess.Read);
 				_partStream.Read(_fragment,0,_force);
 				
-				await target.WriteAsync(_fragment,0,_force);
+				await _targetStream.WriteAsync(_fragment,0,_force);
 				
 				_partStream.Close();
 			}
 			
-			
+			_targetStream.Close();
 			
 		}
 	}
